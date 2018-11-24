b5bc98daa7f0f5b263537f3561b33db8315cf946
まどの <26>
2018-11-25 04:05:41 +0900

    build

--- src/redux/modules/actions.ts
+++ src/redux/modules/actions.ts
@@ -3,9 +3,23 @@ import { Dispatch } from 'redux'
 
 import { ActionType, createStandardAction, getType } from 'typesafe-actions'
 
-interface ICreateShipPayload { shipId: string; masterId: number; fleetId: string; index: number }
+interface ICreateShipPayload {
+  shipId: string
+  masterId: number
+  fleetId: string
+  index: number
+}
 const createShip = createStandardAction('CREATE_SHIP')<ICreateShipPayload>()
 
-const generateShip = (dispatch: Dispatch) => (payload: Omit<ICreateShipPayload, 'shipId'>) {
+const generateShip = (dispatch: Dispatch) => (payload: Omit<ICreateShipPayload, 'shipId'>) => {
   dispatch(createShip({ shipId: Math.random().toString(), ...payload }))
 }
+
+const actions = {
+  createShip,
+  generateShip
+}
+
+export type KcDataBaseAction = ActionType<typeof actions>
+
+export default actions
