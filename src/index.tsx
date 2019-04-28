beff1f0262716467008e88f9796ca440b33314e3
まどの <madonoharu@gmail.com>
2019-04-28 13:28:01 +0900

    core-js追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import 'core-js/es/array/flat'
+import 'core-js/es/array/flat-map'
+
 import App from './App'
 import './index.css'
+
 // import registerServiceWorker from './registerServiceWorker'
 // registerServiceWorker()
 
