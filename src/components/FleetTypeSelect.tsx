fb4ec6d09cff7727c12af96d36d7859ec0bfb0ad
まどの <madonoharu@gmail.com>
2019-10-17 16:24:53 +0900

    fix FleetTypeSelect

--- src/components/FleetTypeSelect.tsx
+++ src/components/FleetTypeSelect.tsx
@@ -1,30 +1,25 @@
-import { FleetTypeName } from "kc-calculator"
+import { FleetTypeName, FleetType } from "kc-calculator"
 import React from "react"
 
-import FormControl from "@material-ui/core/FormControl"
-import MenuItem from "@material-ui/core/MenuItem"
-import Select, { SelectProps } from "@material-ui/core/Select"
+import Select from "./Select"
 
 interface FleetTypeSelectProps {
   fleetType: FleetTypeName
   onChange: (fleetType: FleetTypeName) => void
 }
 
+const types = [
+  FleetTypeName.Single,
+  FleetTypeName.CarrierTaskForce,
+  FleetTypeName.SurfaceTaskForce,
+  FleetTypeName.TransportEscort,
+  FleetTypeName.Combined
+]
+
+const getFleetTypeJp = (key: FleetTypeName) => FleetType[key].name
+
 const FleetTypeSelect: React.FC<FleetTypeSelectProps> = ({ fleetType, onChange }) => {
-  const handleChange = (event: React.ChangeEvent<SelectProps>) => {
-    onChange(event.target.value as FleetTypeName)
-  }
-  return (
-    <FormControl>
-      <Select variant="outlined" value={fleetType} onChange={handleChange}>
-        <MenuItem value={FleetTypeName.Single}>通常艦隊</MenuItem>
-        <MenuItem value={FleetTypeName.CarrierTaskForce}>空母機動部隊</MenuItem>
-        <MenuItem value={FleetTypeName.SurfaceTaskForce}>水上打撃部隊</MenuItem>
-        <MenuItem value={FleetTypeName.TransportEscort}>輸送護衛部隊 </MenuItem>
-        <MenuItem value={FleetTypeName.Combined}>敵連合</MenuItem>
-      </Select>
-    </FormControl>
-  )
+  return <Select value={fleetType} options={types} onChange={onChange} getOptionLabel={getFleetTypeJp} />
 }
 
 export default FleetTypeSelect
